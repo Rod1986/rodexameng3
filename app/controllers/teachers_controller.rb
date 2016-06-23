4e52a1ae9ea4246aa13ff2fbb1559298acae274a
Rodrigo <hein.rodrigo@gmail.com>
2016-06-23 20:20:21 -0300

    pages/index added

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -3,7 +3,7 @@ class TeachersController < InheritedResources::Base
   private
 
     def teacher_params
-      params.require(:teacher).permit(:name, :email, :cv, :picture)
+      params.require(:teacher).permit(:name, :email, :cv, :picture, :ranking)
     end
 end
 
