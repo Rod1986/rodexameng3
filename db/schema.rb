1ecd19a85722a88b0ba1d285c166a9e3a7f15b71
Rodrigo <hein.rodrigo@gmail.com>
2016-06-23 21:24:25 -0300

    roles well-added this time

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623230344) do
+ActiveRecord::Schema.define(version: 20160624002344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "student_teachers", force: :cascade do |t|
     t.string   "question"
@@ -57,11 +63,14 @@ ActiveRecord::Schema.define(version: 20160623230344) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "role_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
 
   add_foreign_key "student_teachers", "students"
   add_foreign_key "student_teachers", "teachers"
+  add_foreign_key "users", "roles"
 end
