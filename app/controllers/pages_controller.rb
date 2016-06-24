84ab9c35f3c82996440981273dd7d3f408d5cb8d
Rodrigo <hein.rodrigo@gmail.com>
2016-06-23 22:37:05 -0300

    last commit: seeds added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def index
+  #@teacherslist = User.where(:role_id => 1).order("id ASC")
+  #@teachers = Kaminari.paginate_array(@teacherslist).page(params[:page]).per(10)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+teacher1 = Teacher.create(name:'prueba1', email:'prueba1@prueba1.cl', password:'password')
+teacher2 = Teacher.create(name:'prueba2', email:'prueba2@prueba2.cl', password:'password')
+student1 = Student.create(name:'prueba3', email:'prueba3@prueba3.cl', password:'password')
